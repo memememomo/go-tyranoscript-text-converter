ebd0fbb2ae9ed535fe71db28aa2535cc01c7ca3f
Uchiko <memememomo@gmail.com>
2020-10-18 19:12:23 +0900

    [add] add test

--- service_test.go
+++ service_test.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestReadAndProcess(t *testing.T) {
-	fmt.Println(readAndProcess("studio_config.json", "target_file1.ks"))
+	actual := readAndProcess("fixtures/studio_config.json", "fixtures/target_file1.ks")
+	assert.Equal(t, `吾輩わがはいは猫である。[r]
+名前はまだ無い。[p]
+
+;ここはコメントです。
+どこで生れたかとんと見当けんとうがつかぬ。[r]
+何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。[r]
+吾輩はここで始めて人間というものを見た。[p]
+
+[chara_mod name="あかね" face="笑顔"]
+`, actual)
 }
